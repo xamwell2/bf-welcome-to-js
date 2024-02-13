099d446f6351e511cdb6a0d0c8d7836bb0139d74
xamwell2 <bxamwell@gmail.com>
2024-02-13 15:18:09 +0100

    adding_devs_and_computers git psuh $ git commit -m adding_devs_and_compute

--- lesson-plans/computers-and-developers/tracing-variables/write-your-examples/5-undefined.js
+++ lesson-plans/computers-and-developers/tracing-variables/write-your-examples/5-undefined.js
@@ -6,3 +6,9 @@ TODO: create a few variables(undefined):
 - you need to add comments to show where the variable been
 declared, initialized, read , reassigned,...
 */
+
+const age = Number(prompt('Enter your age')); //Declaration of "age" variable.
+
+let name = prompt('Enter your name'); // Declaration of "name" variable.
+
+console.log('Hi ' + name + ' your age is ' + age + '.');
